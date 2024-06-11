215dff1ff167563ca0cd50a7fde98f8fb088da2f
AustinZhouOWO <73068151+AustinZhouOWO@users.noreply.github.com>
2024-06-11 13:48:27 -0700

    tweak handlehello func

--- demo/hello.go
+++ demo/hello.go
@@ -13,9 +13,16 @@ func Trim(str string) []string {
 	return strings.Split(str, " ")
 }
 
-func (handler *Handle) HandleHello(w http.ResponseWriter, r *http.Request) *Response {
+func (handler *Handle) HandleHello(w http.ResponseWriter, r *http.Request, params HandleHelloParams) *Response {
+	if params.Name == nil {
+		return &Response{
+			Code:        http.StatusBadRequest,
+			contentType: "no name param",
+		}
+	}
+
 	result := Hello{
-		Name: "Austin Zhou",
+		Name: *params.Name,
 	}
 
 	return HandleHelloJSON200Response(Trim(result.Name))
