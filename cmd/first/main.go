230e743d6e83635a072d431a66e536eb69c0d6c7
AustinZhouOWO <73068151+AustinZhouOWO@users.noreply.github.com>
2024-06-11 13:28:25 -0700

    use demo routes

--- cmd/first/main.go
+++ cmd/first/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/AustinZhouOWO/demo_idm/idm"
+	"github.com/AustinZhouOWO/demo_idm/demo"
 	"github.com/tendant/chi-demo/app"
 )
 
 func main() {
 	myApp := app.Default()
-	// myApp.Config.Metrics.Port = 9901
-	myApp.R.Get("/hello", idm.HandleHello)
+	//myApp.Config.Metrics.Port = 9901
+	handler := demo.Handle{}
+	demo.Routes(myApp.R, handler)
 	myApp.Run()
 }
