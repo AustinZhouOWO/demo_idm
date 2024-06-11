a21e7ff41d25a67bf03161d68334bf191ed99ab8
AustinZhouOWO <73068151+AustinZhouOWO@users.noreply.github.com>
2024-06-11 10:40:49 -0700

    add handler

--- cmd/first/main.go
+++ cmd/first/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/tendant/chi-demo/app"
 )
 
 func main() {
 	myApp := app.Default()
+	// myApp.Config.Metrics.Port = 9901
+	myApp.R.Get("/hello", handleHello)
 	myApp.Run()
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
